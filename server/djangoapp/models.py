0f1381f37df846759d3666abc7a0898aca594ef7
DonZiglioni <Shift2bass@gmail.com>
2023-10-15 19:27:09 -0400

    Third Section

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    car_name = models.CharField(null=False, max_length=30, default='Honda')
+    car_description = models.CharField(null=False, max_length=30, default='Sudan')
+       
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.car_name + " " + self.car_description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,8 +25,20 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
-
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    car_name = models.CharField(null=False, max_length=30, default='Honda')
+    car_type = models.CharField(null=False, max_length=30, default='SUV')
+    car_year = models.DateField(null=False, default=2005)
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Car Make: " + self.car_make + ", " + \
+               "Dealer ID: " + str(self.dealer_id) + ", " + \
+               "Car Name: " + self.car_name + ", " + \
+               "Car Type: " + self.total_learners + ", " + \
+               "Car Year: " + str(self.total_learners)
+                
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
